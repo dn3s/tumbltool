e194f551077f65b13ccb629cba6a762251a6f799
dn3s <d@devonsawatzky.ca>
2015-03-31 22:07:51 -0500

    cleaned up tag parsing module

--- TumblTool/TumblrTags.pm
+++ TumblTool/TumblrTags.pm
@@ -3,14 +3,6 @@ use strict;
 package TumblTool::TumblrTags;
 use TumblTool::TextTransforms;
 use Data::Dumper;
-sub printVar
-{
-	(my $var, my $content)=@_;
-	if(0 and $var->{""} eq "TagsAsClasses") {
-		return join(" ", $content->{"Tags"}); #todo: make this safe
-	}
-	return;
-}
 sub wrangleVars
 {
 	(my $content)=@_;
@@ -19,12 +11,12 @@ sub wrangleVars
 		my $structuredTags=[];
 		my $tagsAsClasses="";
 		foreach my $tag (@{$tags}) {
+			#TODO: properly sanitize tags for URLs and class names
 			my $hash={"Tag"=>$tag, "TagURL"=>"http://example.com/$tag"};
 			push($structuredTags,$hash);
-			$tagsAsClasses.=" $tag";
 		}
 		$content->{"Tags"}=$structuredTags;
-		$content->{"TagsAsClasses"}=$tagsAsClasses;
+		$content->{"TagsAsClasses"}=join(" ", $tags);
 	}
 	return $content;
 }
