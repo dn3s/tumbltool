747cd0ffd1458aff0f61e3e2abf65ddb95b8309e
dn3s <d@devonsawatzky.ca>
2015-03-31 22:14:19 -0500

    fixed trailing closing div tag in reblog button

--- TumblTool/TumblrTags.pm
+++ TumblTool/TumblrTags.pm
@@ -9,14 +9,13 @@ sub wrangleVars
 	my $tags=$content->{"Tags"};
 	if($tags and !(ref(@{$tags}[0]) eq "HASH")) {
 		my $structuredTags=[];
-		my $tagsAsClasses="";
 		foreach my $tag (@{$tags}) {
 			#TODO: properly sanitize tags for URLs and class names
 			my $hash={"Tag"=>$tag, "TagURL"=>"http://example.com/$tag"};
-			push($structuredTags,$hash);
+			push(@{$structuredTags}, $hash);
 		}
 		$content->{"Tags"}=$structuredTags;
-		$content->{"TagsAsClasses"}=join(" ", $tags);
+		$content->{"TagsAsClasses"}=join(" ", @{$tags});
 	}
 	return $content;
 }
