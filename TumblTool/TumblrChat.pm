e8a1550907b1ef20190c14673c8f6feb4afd9307
dn3s <d@devonsawatzky.ca>
2015-04-11 17:39:28 -0500

    updated chat to better match Tumblr

--- TumblTool/TumblrChat.pm
+++ TumblTool/TumblrChat.pm
@@ -8,14 +8,14 @@ sub processContent
 	for my $post (@{$content->{"Posts"}})
 	{
 		if($post->{"PostType"} and $post->{"PostType"} eq "chat" and $post->{"Lines"}) {
-			processLines($post->{"Lines"});
+			processLines($post->{"Lines"}, $post->{"PostAuthor"});
 		}
 	}
 }
 
 sub processLines
 {
-	(my $lines)=@_;
+	(my $lines, my $name)=@_;
 	my $users={};
 	my $nextId=1;
 	my $odd=1;
@@ -23,6 +23,7 @@ sub processLines
 		$line->{"Alt"}=$odd?"odd":"even";
 		$odd=!$odd;
 		my $label=$line->{"Label"};
+		$line->{"name"}=$name;
 		if($label) {
 			if(!$users->{$label}) {
 				$users->{$label}="$nextId";
